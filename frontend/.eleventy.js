dca83f6436be2f58da0332d9896be13f8f8e9525
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2023-10-26 16:50:10 +0100

    feat(frontend): add support for draft markdown content

--- frontend/.eleventy.js
+++ frontend/.eleventy.js
@@ -76,6 +76,39 @@ module.exports = (eleventyConfig) => {
     });
   });
 
+  // Draft posts https://www.11ty.dev/docs/quicktips/draft-posts/
+  // When `permalink` is false, the file is not written to disk
+  eleventyConfig.addGlobalData("eleventyComputed.permalink", function () {
+    return (data) => {
+      // Always skip during non-watch/serve builds
+      if (data.draft && !process.env.BUILD_DRAFTS) {
+        return false;
+      }
+
+      return data.permalink;
+    };
+  });
+  // When `eleventyExcludeFromCollections` is true, the file is not included in any collections
+  eleventyConfig.addGlobalData(
+    "eleventyComputed.eleventyExcludeFromCollections",
+    function () {
+      return (data) => {
+        // Always exclude from non-watch/serve builds
+        if (data.draft && !process.env.BUILD_DRAFTS) {
+          return true;
+        }
+
+        return data.eleventyExcludeFromCollections;
+      };
+    }
+  );
+  eleventyConfig.on("eleventy.before", ({ runMode }) => {
+    // Set the environment variable
+    if (runMode === "serve" || runMode === "watch") {
+      process.env.BUILD_DRAFTS = true;
+    }
+  });
+
   eleventyConfig.addFilter("toLocaleDate", kdlFilters.toLocaleDate);
   eleventyConfig.addFilter("filter", kdlFilters.filter);
   eleventyConfig.addFilter("renderMd", kdlFilters.renderMd);
