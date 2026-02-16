955f12fa98567c3820a857407125ccbe80b4e7ab
Stefan Meier <stefan.meier@kcl.ac.uk>
2026-02-16 14:29:43 +0000

    build: remove call to directus library

--- frontend/.eleventy.js
+++ frontend/.eleventy.js
@@ -7,7 +7,6 @@ const markdownItAnchor = require("markdown-it-anchor");
 const markdownItAttrs = require("markdown-it-attrs");
 const pluginSEO = require("eleventy-plugin-seo");
 const pluginTOC = require("eleventy-plugin-toc");
-const { Directus } = require("@directus/sdk");
 const markdownIt = require("markdown-it");
 const Nunjucks = require("nunjucks");
 const path = require("node:path");
@@ -15,262 +14,231 @@ const sass = require("sass");
 
 require("dotenv").config();
 
-function getDirectus() {
-  return new Directus(process.env.DIRECTUS_URL);
-}
-
 module.exports = (eleventyConfig) => {
-  const kdlComponentsPath = "../node_modules/kdl-components/src";
-  const nunjucksEnvironment = new Nunjucks.Environment([
-    new Nunjucks.FileSystemLoader(kdlComponentsPath),
-    new Nunjucks.FileSystemLoader("./src/_includes"),
-  ]);
-  eleventyConfig.setLibrary("njk", nunjucksEnvironment);
-  eleventyConfig.addWatchTarget(kdlComponentsPath);
+	const kdlComponentsPath = "../node_modules/kdl-components/src";
+	const nunjucksEnvironment = new Nunjucks.Environment([
+		new Nunjucks.FileSystemLoader(kdlComponentsPath),
+		new Nunjucks.FileSystemLoader("./src/_includes"),
+	]);
+	eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+	eleventyConfig.addWatchTarget(kdlComponentsPath);
 
-  eleventyConfig.setTemplateFormats(["html", "njk", "md"]);
-  eleventyConfig.addPassthroughCopy({
-    [`${kdlComponentsPath}/kdl/assets`]: "/assets",
-    "../node_modules/reveal.js/dist": "assets/reveal/",
-    "../node_modules/reveal.js/plugin": "assets/reveal/plugin",
-    public: "/",
-  });
+	eleventyConfig.setTemplateFormats(["html", "njk", "md"]);
+	eleventyConfig.addPassthroughCopy({
+		[`${kdlComponentsPath}/kdl/assets`]: "/assets",
+		"../node_modules/reveal.js/dist": "assets/reveal/",
+		"../node_modules/reveal.js/plugin": "assets/reveal/plugin",
+		public: "/",
+	});
 
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-  eleventyConfig.addPlugin(pluginEleventyNavigation);
-  eleventyConfig.addPlugin(pluginMermaid);
-  eleventyConfig.addPlugin(pluginSEO, require("./src/_data/config.js"));
-  eleventyConfig.addPlugin(pluginTOC);
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+	eleventyConfig.addPlugin(pluginEleventyNavigation);
+	eleventyConfig.addPlugin(pluginMermaid);
+	eleventyConfig.addPlugin(pluginSEO, require("./src/_data/config.js"));
+	eleventyConfig.addPlugin(pluginTOC);
 
-  eleventyConfig.setLibrary(
-    "md",
-    markdownIt({ html: true })
-      .use(markdownItAnchor)
-      .use(markdownItAttrs)
-      .use(markdownItImplicitFigures, {
-        figcaption: true,
-        copyAttrs: "class",
-      })
-  );
+	eleventyConfig.setLibrary(
+		"md",
+		markdownIt({ html: true })
+			.use(markdownItAnchor)
+			.use(markdownItAttrs)
+			.use(markdownItImplicitFigures, {
+				figcaption: true,
+				copyAttrs: "class",
+			}),
+	);
 
-  // TODO: remove this once we no longer need to use the Directus API
-  eleventyConfig.addGlobalData("directus", getDirectus);
-  eleventyConfig.addCollection("projectsByDate", function (collectionApi) {
-    return [...collectionApi.getFilteredByTag("projects")].sort((a, b) => {
-      const dateDescending = (field, a, b) => {
-        if (a.data[field] && b.data[field]) {
-          return new Date(b.data[field]) - new Date(a.data[field]);
-        } else if (a.data[field]) {
-          return -1;
-        } else if (b.data[field]) {
-          return 1;
-        }
+	// Draft posts https://www.11ty.dev/docs/quicktips/draft-posts/
+	// When `permalink` is false, the file is not written to disk
+	eleventyConfig.addGlobalData("eleventyComputed.permalink", function () {
+		return (data) => {
+			// Always skip during non-watch/serve builds
+			if (data.draft && !process.env.BUILD_DRAFTS) {
+				return false;
+			}
 
-        return 0;
-      };
+			return data.permalink;
+		};
+	});
+	// When `eleventyExcludeFromCollections` is true, the file is not included in any collections
+	eleventyConfig.addGlobalData(
+		"eleventyComputed.eleventyExcludeFromCollections",
+		function () {
+			return (data) => {
+				// Always exclude from non-watch/serve builds
+				if (data.draft && !process.env.BUILD_DRAFTS) {
+					return true;
+				}
 
-      const compareDissolution = dateDescending("dissolutionDate", a, b);
-      const compareFounding = dateDescending("foundingDate", a, b);
+				return data.eleventyExcludeFromCollections;
+			};
+		},
+	);
+	eleventyConfig.on("eleventy.before", ({ runMode }) => {
+		// Set the environment variable
+		if (runMode === "serve" || runMode === "watch") {
+			process.env.BUILD_DRAFTS = true;
+		}
+	});
 
-      return (
-        compareDissolution ||
-        compareFounding ||
-        a.data.title.localeCompare(b.data.title)
-      );
-    });
-  });
+	eleventyConfig.addFilter("toLocaleDate", kdlFilters.toLocaleDate);
+	eleventyConfig.addFilter("filter", kdlFilters.filter);
+	eleventyConfig.addFilter("renderMd", kdlFilters.renderMd);
 
-  // Draft posts https://www.11ty.dev/docs/quicktips/draft-posts/
-  // When `permalink` is false, the file is not written to disk
-  eleventyConfig.addGlobalData("eleventyComputed.permalink", function () {
-    return (data) => {
-      // Always skip during non-watch/serve builds
-      if (data.draft && !process.env.BUILD_DRAFTS) {
-        return false;
-      }
+	eleventyConfig.addFilter("asToc", (content, tags = ["h2", "h3", "h4"]) => {
+		const tocFilter = eleventyConfig.getFilter("toc");
 
-      return data.permalink;
-    };
-  });
-  // When `eleventyExcludeFromCollections` is true, the file is not included in any collections
-  eleventyConfig.addGlobalData(
-    "eleventyComputed.eleventyExcludeFromCollections",
-    function () {
-      return (data) => {
-        // Always exclude from non-watch/serve builds
-        if (data.draft && !process.env.BUILD_DRAFTS) {
-          return true;
-        }
+		const toc = tocFilter(content, { tags });
+		if (toc) {
+			const items = toc.split("<li>");
 
-        return data.eleventyExcludeFromCollections;
-      };
-    }
-  );
-  eleventyConfig.on("eleventy.before", ({ runMode }) => {
-    // Set the environment variable
-    if (runMode === "serve" || runMode === "watch") {
-      process.env.BUILD_DRAFTS = true;
-    }
-  });
+			if (items.length > 2) {
+				return toc;
+			}
+		}
 
-  eleventyConfig.addFilter("toLocaleDate", kdlFilters.toLocaleDate);
-  eleventyConfig.addFilter("filter", kdlFilters.filter);
-  eleventyConfig.addFilter("renderMd", kdlFilters.renderMd);
+		return undefined;
+	});
 
-  eleventyConfig.addFilter("asToc", (content, tags = ["h2", "h3", "h4"]) => {
-    const tocFilter = eleventyConfig.getFilter("toc");
+	eleventyConfig.addFilter("featuredPosts", function (posts, number = 3) {
+		return posts.reverse().slice(0, number);
+	});
 
-    const toc = tocFilter(content, { tags });
-    if (toc) {
-      const items = toc.split("<li>");
+	eleventyConfig.addFilter(
+		"featuredProjects",
+		function (projects, status = "Active", number = 3) {
+			return (
+				projects
+					.filter((project) => project.data.creativeWorkStatus === status)
+					.filter((project) => project.data.feature?.image)
+					// .filter((project) => project.content && project.content.length > 0)
+					.sort(() => Math.random() - 0.5)
+					.slice(0, number)
+			);
+		},
+	);
 
-      if (items.length > 2) {
-        return toc;
-      }
-    }
+	eleventyConfig.addFilter("fundedProjects", (projects, agentSlug) => {
+		if (!projects || !agentSlug) return null;
 
-    return undefined;
-  });
+		return projects.filter((project) => {
+			return (
+				project?.data?.fundersSlugs &&
+				project.data.fundersSlugs.some((funder) => funder === agentSlug)
+			);
+		});
+	});
 
-  eleventyConfig.addFilter("featuredPosts", function (posts, number = 3) {
-    return posts.reverse().slice(0, number);
-  });
+	eleventyConfig.addFilter("partnerProjects", (projects, agentSlug) => {
+		if (!projects || !agentSlug) return null;
 
-  eleventyConfig.addFilter(
-    "featuredProjects",
-    function (projects, status = "Active", number = 3) {
-      return (
-        projects
-          .filter((project) => project.data.creativeWorkStatus === status)
-          .filter((project) => project.data.feature?.image)
-          // .filter((project) => project.content && project.content.length > 0)
-          .sort(() => Math.random() - 0.5)
-          .slice(0, number)
-      );
-    }
-  );
+		return projects.filter((project) => {
+			return (
+				project?.data?.departmentsSlugs &&
+				project.data.departmentsSlugs.some(
+					(department) => department === agentSlug,
+				)
+			);
+		});
+	});
 
-  eleventyConfig.addFilter("fundedProjects", (projects, agentSlug) => {
-    if (!projects || !agentSlug) return null;
+	eleventyConfig.addFilter("getAgentProjects", (projects, slug) =>
+		projects
+			.filter(
+				(project) =>
+					project.data.members &&
+					project.data.members.some((member) => member.slug === slug),
+			)
+			.map((project) => ({
+				...project,
+				role: project.data.members.find((member) => member.slug === slug),
+				dissolutionDate: project.data.dissolutionDate || "1970-01-01",
+			})),
+	);
 
-    return projects.filter((project) => {
-      return (
-        project?.data?.fundersSlugs &&
-        project.data.fundersSlugs.some((funder) => funder === agentSlug)
-      );
-    });
-  });
+	eleventyConfig.addFilter("getAgentOrganisations", (organisations, slug) =>
+		Object.values(
+			organisations
+				.filter(
+					(organisation) =>
+						organisation.data.members &&
+						organisation.data.members.some((member) => member.slug === slug),
+				)
+				.reduce((acc, organisation) => {
+					if (!acc[organisation.data.agent]) {
+						acc[organisation.data.agent] = organisation;
+					}
 
-  eleventyConfig.addFilter("partnerProjects", (projects, agentSlug) => {
-    if (!projects || !agentSlug) return null;
+					return acc;
+				}, {}),
+		),
+	);
 
-    return projects.filter((project) => {
-      return (
-        project?.data?.departmentsSlugs &&
-        project.data.departmentsSlugs.some(
-          (department) => department === agentSlug
-        )
-      );
-    });
-  });
+	eleventyConfig.addFilter(
+		"getThemeProjects",
+		function (projects, keywords = []) {
+			return projects
+				.filter((project) =>
+					project.keywords?.some((keyword) =>
+						keywords.includes(keyword.definedterm_id.name),
+					),
+				)
+				.sort((project) => project.name);
+		},
+	);
 
-  eleventyConfig.addFilter("getAgentProjects", (projects, slug) =>
-    projects
-      .filter(
-        (project) =>
-          project.data.members &&
-          project.data.members.some((member) => member.slug === slug)
-      )
-      .map((project) => ({
-        ...project,
-        role: project.data.members.find((member) => member.slug === slug),
-        dissolutionDate: project.data.dissolutionDate || "1970-01-01",
-      }))
-  );
+	eleventyConfig.addShortcode("route", function (path, navigationKey = "") {
+		let url = path;
 
-  eleventyConfig.addFilter("getAgentOrganisations", (organisations, slug) =>
-    Object.values(
-      organisations
-        .filter(
-          (organisation) =>
-            organisation.data.members &&
-            organisation.data.members.some((member) => member.slug === slug)
-        )
-        .reduce((acc, organisation) => {
-          if (!acc[organisation.data.agent]) {
-            acc[organisation.data.agent] = organisation;
-          }
+		if (navigationKey) {
+			const graph = pluginEleventyNavigation.navigation.getDependencyGraph(
+				this.ctx.collections.all,
+			);
 
-          return acc;
-        }, {})
-    )
-  );
+			try {
+				const found = graph.getNodeData(navigationKey);
+				if (found) {
+					url = `${found.url}${path}`;
+				}
+			} catch (error) {
+				console.error("Failed to get route for", navigationKey, error);
+			}
+		}
 
-  eleventyConfig.addFilter(
-    "getThemeProjects",
-    function (projects, keywords = []) {
-      return projects
-        .filter((project) =>
-          project.keywords?.some((keyword) =>
-            keywords.includes(keyword.definedterm_id.name)
-          )
-        )
-        .sort((project) => project.name);
-    }
-  );
+		return url;
+	});
 
-  eleventyConfig.addShortcode("route", function (path, navigationKey = "") {
-    let url = path;
+	eleventyConfig.addPairedShortcode(
+		"slide",
+		function (content, options = "data-auto-animate") {
+			return `<section class="slide" ${options.split(
+				",",
+			)}>${content}</section>`;
+		},
+	);
 
-    if (navigationKey) {
-      const graph = pluginEleventyNavigation.navigation.getDependencyGraph(
-        this.ctx.collections.all
-      );
+	// https://www.11ty.dev/docs/languages/custom/#example-add-sass-support-to-eleventy
+	eleventyConfig.addTemplateFormats("scss");
+	eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+		style: "compressed",
+		compile: async function (inputContent, inputPath) {
+			const parsed = path.parse(inputPath);
+			const result = sass.compileString(inputContent, {
+				loadPaths: [parsed.dir || ".", this.config.dir.includes],
+			});
 
-      try {
-        const found = graph.getNodeData(navigationKey);
-        if (found) {
-          url = `${found.url}${path}`;
-        }
-      } catch (error) {
-        console.error("Failed to get route for", navigationKey, error);
-      }
-    }
+			return (_) => {
+				return result.css;
+			};
+		},
+	});
 
-    return url;
-  });
-
-  eleventyConfig.addPairedShortcode(
-    "slide",
-    function (content, options = "data-auto-animate") {
-      return `<section class="slide" ${options.split(
-        ","
-      )}>${content}</section>`;
-    }
-  );
-
-  // https://www.11ty.dev/docs/languages/custom/#example-add-sass-support-to-eleventy
-  eleventyConfig.addTemplateFormats("scss");
-  eleventyConfig.addExtension("scss", {
-    outputFileExtension: "css",
-    style: "compressed",
-    compile: async function (inputContent, inputPath) {
-      const parsed = path.parse(inputPath);
-      const result = sass.compileString(inputContent, {
-        loadPaths: [parsed.dir || ".", this.config.dir.includes],
-      });
-
-      return (_) => {
-        return result.css;
-      };
-    },
-  });
-
-  return {
-    dir: {
-      input: "src",
-      includes: "_includes",
-      output: "_site",
-    },
-  };
+	return {
+		dir: {
+			input: "src",
+			includes: "_includes",
+			output: "_site",
+		},
+	};
 };
